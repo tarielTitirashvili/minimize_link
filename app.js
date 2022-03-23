fb5ea59a6df5e2c0459cb0ebd9543d361aa7076f
Tariel <77127150+tarielTitirashvili@users.noreply.github.com>
2022-03-23 22:10:28 +0400

    links methods

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const mongoose = require('mongoose')
 const app = express()
 app.use(express.json({extended: true}))
 
-app.use('/api/auth', require('./routes/auth'))
+app.use('/api/auth', require('./routes/auth.routes'))
+app.use('/api/links', require('./routes/link.routes'))
+
 async function start() {
   try{
     await mongoose.connect(config.get('mongoUri', {
