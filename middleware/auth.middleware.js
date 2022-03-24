1ba741a899a74f44863fe6fb3215d18bd97da14c
Tariel <77127150+tarielTitirashvili@users.noreply.github.com>
2022-03-24 23:47:55 +0400

    fixing auth middleware

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,5 @@
-const config = require('config')
 const jwt = require('jsonwebtoken')
+const config = require('config')
 
 module.exports= (req, res, next) =>{
   if(req.method === 'OPTIONS'){
@@ -7,12 +7,13 @@ module.exports= (req, res, next) =>{
   }
 
   try{
-    const token = req.headers.authorization.split()[1]
+    const token = req.headers.authorization.split(' ')[1]
+    console.log(req.headers.authorization)
     if(!token){
       res.status(401).json({message: 'need authentication'})
     }
     const decode = jwt.verify(token, config.get('jwtSecretKey'))
-    req.user = decoded
+    req.user = decode
     next()
 
   }catch(e){
